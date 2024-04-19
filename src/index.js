5bca82ac12b0346dde69c228689fd8181094c47f
Nilesh <nileshraut114@gmail.com>
2024-04-19 06:55:40 +0000

    update

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
-// import express from "express";
-// const app=express()
+import express from "express";
+import jwt from "jsonwebtoken";
+const app=express()
 
-// app.use("/",(req,res)=>{
-// res.send("asdasd")
-// })
-// app.listen(8000)
+app.use("/login/:name/:id",(req,res)=>{
+    const privateKey="asdasdadsd326teyeqwd76q3teqw7e7q278e2ye8y7";
+    console.log(req.params);
+    const token=jwt.sign({ foo: req.params.name }, privateKey);
+    res.send(token)
+})
+app.listen(8000)
 
-import { createServer } from "http"
-import {Server} from "socket.io"
-const server=createServer().listen(3000)
-const io=new Server(server)
+// import { createServer } from "http"
+// import {Server} from "socket.io"
+// const server=createServer().listen(3000)
+// const io=new Server(server)
 
 
