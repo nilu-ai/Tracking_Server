e1a11cf7858b296276a1a17cb91386d7865ff267
Nilesh <nileshraut114@gmail.com>
2024-04-22 10:39:50 +0000

    Cookies learing phase completed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,11 @@
 import express from "express";
 import jwt from "jsonwebtoken";
-const app=express()
 
-app.use("/login/:name/:id",(req,res)=>{
-    const privateKey="asdasdadsd326teyeqwd76q3teqw7e7q278e2ye8y7";
-    console.log(req.params);
-    const token=jwt.sign({ foo: req.params.name }, privateKey);
-    res.send(token)
-})
+const app=express()
 app.listen(8000)
+import user from "./routes.js";
+app.use("/user",user)
+
 
 // import { createServer } from "http"
 // import {Server} from "socket.io"
---
+++ src/routes.js
@@ -0,0 +1,19 @@
+import { Router } from "express";
+import jwt from "jsonwebtoken";
+const router=Router()
+router.route("/login/:name/:pass").get((req,res)=>{
+    console.log("this calling");
+    const privateKey="asdasdadsd326teyeqwd76q3teqw7e7q278e2ye8y7";
+    console.log(req.params);
+    const token=jwt.sign({ foo: req.params.pass }, privateKey);
+    res.cookie("Token_cookies",token,{expires: new Date(Date.now() + 86400000)    })
+    res.send("Cookiew are setted")
+})
+      
+
+router.route("/logout").get((req,res)=>{
+    console.log("this calling");
+    res.send("You on login reques")
+})
+
+export default router;
