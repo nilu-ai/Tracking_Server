8020967e1d22d0012a4c357991e2c99fbc96d95d
Nilesh <nileshraut114@gmail.com>
2024-04-25 06:22:11 +0000

    add layer of recommedataion to main backend layer

--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,52 @@ router.route("/logout").get((req,res)=>{
     console.log("this calling");
     res.clearCookie("Token_cookies")
     res.send("log out")
+})
 
+const data = {
+    "a": ["apple", "avocado"],
+    "b": ["banana", "blueberry"],
+    "c": ["cherry", "coconut"],
+    "d": ["date", "dragonfruit"],
+    "e": ["elderberry", "eggplant"],
+    "f": ["fig", "feijoa"],
+    "g": ["grape", "guava"],
+    "h": ["honeydew", "huckleberry"],
+    "i": ["kiwi", "imbe"],
+    "j": ["jackfruit", "jujube"],
+    "k": ["kiwano", "kumquat"],
+    "l": ["lemon", "lime"],
+    "m": ["mango", "mulberry"],
+    "n": ["nectarine", "nutmeg"],
+    "o": ["orange", "olive"],
+    "p": ["papaya", "passionfruit"],
+    "q": ["quince", "quince"],
+    "r": ["raspberry", "rambutan"],
+    "s": ["strawberry", "soursop"],
+    "t": ["tangerine", "tamarillo"],
+    "u": ["ugli fruit", "uvaia"],
+    "v": ["vanilla", "victoria plum"],
+    "w": ["watermelon", "wolfberry"],
+    "x": ["xigua", "ximenia"],
+    "y": ["yuzu", "yumberry"],
+    "z": ["zucchini", "zinfandel grape"]
+};
+
+router.route("/recommedation/:name").get((req,res)=>{
+    const check= req.params.name;
+    let recommedation=data[check[0]]
+    res.send(recommedation)
+})
+
+router.route("/cookies").get((req,res)=>{
+    const cookieString=req.get("cookie")
+    const token = cookieString.substring('Token_cookies='.length);
+    const privateKey="asdasdadsd326teyeqwd76q3teqw7e7q278e2ye8y7";
+
+    const data=jwt.verify(token,privateKey)
+    console.log(data);
+
+    res.send(`Checking Your Cookies status and send the result:${cookieString} , UserName:${data.foo} `)
 })
 
 router.route("/send/:message/:id").get(auth,(req,res)=>{
